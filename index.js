4ec5932a211a67983709b9ee4f1a7d899ba0b148
Natalie Burgei <natalieburgei04@gmail.com>
2023-10-06 14:53:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats =["Milo", "Otis", "Garfield"]
+console.log(cats)
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+ function destructivelyPrependCat(name) {
+     cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function  destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function  appendCat(name){
+const Broom=[...cats,name]
+return Broom;
+}
+function prependCat(name){
+
+    const Arnold=[name,...cats]
+    return Arnold;
+}
+function removeLastCat(){
+    const newArray=[...cats]
+    newArray.pop()
+    return newArray;
+}
+function  removeFirstCat(){
+    const newArray=[...cats]
+
+    newArray.shift()
+    return newArray;
+}
